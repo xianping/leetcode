96511f0467684c17a141e12a27055b6e1bd4d8b0
xianping <linxpwww@163.com>
2014-12-29 10:33:24 +0800

    Update gasStation.cpp

--- gasStation/gasStation.cpp
+++ gasStation/gasStation.cpp
@@ -1,3 +1,7 @@
+/**
+ * 首先确认gas总和大于cost，因此判断能够绕圈。接下来寻找起始位置，我们可以借鉴归并排序的思路，如果某一段路gas>cost，则这段路剩余的油量可以支撑其他路段。因此问题变化为找到某个节点，在它之前的路段剩余油量为负，而从它开始到整个队列结束剩余油量为正（正油量可以不足前面路段的不足油量）。时间可以在O（n）完成。
+ * 
+ * */
 class Solution {
 public:
     int canCompleteCircuit(vector<int> &gas, vector<int> &cost) {
